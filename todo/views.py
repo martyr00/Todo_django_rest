659ff62f2120073a1d10e15fbf0948d644ed3eec
martyr00 <leonyvb04@gmail.com>
2023-06-22 08:01:46 +0300

    fix get_queryset for admin

--- todo/views.py
+++ todo/views.py
@@ -1,24 +1,9 @@
-from django.db.models import Prefetch
-from django.http import HttpResponse
-from rest_framework.response import Response
+from .models import TodoCard
+from rest_framework import generics
 
 from .models import TodoCard
 from .permission import IsOwnerOrAdmin
 from .serializers import TodoCardSerializer
-from rest_framework import viewsets, generics
-from django.contrib.auth.models import User
-
-# class TodoCardViewSet(viewsets.ModelViewSet):
-#     queryset = TodoCard.objects.all()
-#     serializer_class = TodoCardSerializer
-#
-#     def get_queryset(self):
-#         pk = self.kwargs.get('pk')
-#
-#         if not pk:
-#             return TodoCard.objects.all()
-#
-#         return TodoCard.objects.filter(pk=pk)
 
 
 class TodoCardAPIListCreate(generics.ListCreateAPIView):
@@ -39,4 +24,3 @@ class TodoCardAPIRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = TodoCard.objects.all()
     serializer_class = TodoCardSerializer
     permission_classes = (IsOwnerOrAdmin, )
-
